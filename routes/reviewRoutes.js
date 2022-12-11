d99ade9cae5d938c998e1f94fc95f41e0239af52
Hakan Baştürk <forxynet@gmail.com>
2022-12-11 15:20:34 +0300

    finished final considerations

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router({ mergeParams: true });
 
@@ -8,7 +9,7 @@ router.use(authController.protect);
 
 router
   .route('/')
-  .get(reviewController.getAllReviews)
+  .get(bookingController.createBookingCheckout, reviewController.getAllReviews)
   .post(
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
